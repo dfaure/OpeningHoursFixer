0a1852a92b9286b2f1452cb5b23a328306857317
David Faure <faure@kde.org>
2021-01-16 09:47:25 +0100

    Specify source=OSM-data in the changesets
    The default is survey, which is completely wrong.
    Reported that as a bug:
    https://github.com/grigory-rechistov/osm-bulk-upload/issues/2

--- upload_changes.sh
+++ upload_changes.sh
@@ -15,5 +15,5 @@ for changeset in changes/*.osc; do
     comment="Fixing opening_hours syntax using KOpeningHours and manual review"
 
     ../osm-bulk-upload/upload.py -u davidfaure_bot -p $password -c yes -m "$comment" $changeset \
-    -x "OpeningHoursFixer $version, via osm-bulk-upload/python.py" -z "https://github.com/dfaure/OpeningHoursFixer"
+    -x "OpeningHoursFixer $version, via osm-bulk-upload/python.py" -z "https://github.com/dfaure/OpeningHoursFixer" -y "OSM-data"
 done
